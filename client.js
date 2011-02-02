c9318a771f0226e4277ca771bcbeef43a7d39707
tjgillies <tjgillies@gmail.com>
2011-02-02 06:23:11 -0800

    cleaned up client.js

--- client.js
+++ client.js
@@ -2,12 +2,6 @@ var dgram = require('dgram');
 var crypto = require('crypto');
 var fs = require('fs');
 var ezcrypto = require('./ezcrypto-js/ezcrypto.js').ezcrypto;
-//var process = require('process');
-
-//var keyPem = fs.readFileSync("mykey.pem", 'ascii');
-//var pubKey = fs.readFileSync("mycert.pem", 'ascii');
-//var cred = crypto.createCredentials({key:keyPem});
-//var key = cred.key;
 try { 
   data = fs.readFileSync('./keys','ascii')
   keys = JSON.parse(data);
@@ -18,12 +12,8 @@ try {
 }
 
 var socket = dgram.createSocket("udp4");
-//var signer = crypto.createSign('RSA-SHA1');
 var msg = process.argv[2];
-//signer.update(msg);
-//var signature = signer.sign(keyPem, output_format='hex');
 var hash = ezcrypto.hash(msg);
-console.log(hash);
 var signature = ezcrypto.sign(hash, keys.public, keys.private);
 var json = {"+key":keys.public, "_hop":1,"+end":"8bf1cce916417d16b7554135b6b075fb16dd26ce","_to":"208.68.163.247:42424", "+sig":signature, "+message":msg};
 var message = new Buffer(JSON.stringify(json));
